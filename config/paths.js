453e5e5498b2b78f651349053abb314092b79091
bibi <me@pannnda.com>
2019-05-05 14:12:55 +0800

    配置多入口

--- config/paths.js
+++ config/paths.js
@@ -72,6 +72,7 @@ module.exports = {
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
+  mobileHtml: resolveApp('public/mobile.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -79,6 +79,12 @@ module.exports = function(proxy, allowedHost) {
       // Paths with dots should still use the history fallback.
       // See https://github.com/facebook/create-react-app/issues/387.
       disableDotRule: true,
+      // rewrites: [
+      //   {
+      //     from: /^\/mobile.html/,
+      //     to: '/build/mobile.html'
+      //   }
+      // ]
     },
     public: allowedHost,
     proxy,
