f85b78a799609d16d18bd8bb151f23c257078213
bibi <me@pannnda.com>
2019-05-05 22:35:06 +0800

    完成index轮播

--- src/pc/pages/Index/index.js
+++ src/pc/pages/Index/index.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import Slider from 'react-slick'
 import './index.scss'
 
 export const Index = props => {
+  const setting = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: 'max',
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 6000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+  }
   
   return (
     <div className={`index page ${props.className}`}>
-      index
+      <Slider {...setting}>
+        <div>
+          <img src={require('../../../assets/banner/1.png')} />
+        </div>
+        <div>
+          <img src={require('../../../assets/banner/2.png')} />
+        </div>
+        <div>
+          <img src={require('../../../assets/banner/3.png')} />
+        </div>
+      </Slider>
     </div>
+  )
+}
+
+const PrevArrow = props => {
+  const { onClick } = props
+  return (
+    <div
+      className="prev button"
+      onClick={onClick}
+    />
+  )
+}
+
+const NextArrow = props => {
+  const { onClick } = props
+  return (
+    <div
+      className="next button"
+      onClick={onClick}
+    />
   )
 }
